d21f22b55de302a8fc803b543a39199959a78018
Frank <fsr32@cornell.edu>
2017-12-23 00:28:06 -0500

    Added many new developments, such as a finished about section and social section

--- controller.js
+++ controller.js
@@ -3,9 +3,9 @@ window.onload = () => {
   const navItems = document.querySelectorAll('.nav-item');
   const hamburger = document.querySelector('.nav-toggle');
   
-  const toggle = e => e.classList.toggle('is-active');
-  const toggleNav = ({ target }) => Array.from(navMenu.classList).includes('is-active') ? toggle(navMenu) : null;
+  // const toggle = e => e.classList.toggle('is-active');
+  // const toggleNav = ({ target }) => Array.from(navMenu.classList).includes('is-active') ? toggle(navMenu) : null;
 
   hamburger.addEventListener('click', () => toggle(navMenu, 'is-active'));
-  Array.from(navItems).forEach(e => e.addEventListener('click', toggleNav));
+  // Array.from(navItems).forEach(e => e.addEventListener('click', toggleNav));
 }
